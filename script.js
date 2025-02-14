340c9fc3250e777b6c4dd7409a81b4121ac4a892
MDILLIPRASAD <dilliprasadreddy0000@gmail.com>
2025-02-14 17:46:19 +0530

    Add hoisting explanation and basic HTML structure for JavaScript practice

---
+++ script.js
@@ -0,0 +1,5 @@
+heading = document.createElement("h1");
+heading.textContent = "This is JAVASCRIPT PRACTICE";
+heading.id = "heading";
+document.body.appendChild(heading);
+document.getElementById("heading").style.color = "red";
